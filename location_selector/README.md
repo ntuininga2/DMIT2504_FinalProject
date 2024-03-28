20c65e3e266b5611981348b27b6729ec7cb09246
ntuininga2 <122303014+ntuininga2@users.noreply.github.com>
2024-03-28 10:53:51 -0600

    initial commit

---
+++ location_selector/README.md
@@ -0,0 +1,3 @@
+# location_selector
+
+A new Flutter project.
---
+++ location_selector/android/app/src/main/kotlin/com/example/location_selector/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.location_selector
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
